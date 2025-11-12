55736b77e5166b79ec73e0187aa500deb3bff4cb
Yahir Eduardo Bravo <yahir.eduardo.bravo@gmail.com>
2025-11-12 15:46:28 -0500

    feat(plugins): change the blink configuration

---
+++ lua/plugins/blink.lua
@@ -0,0 +1,53 @@
+return { -- override blink.cmp plugin
+  "Saghen/blink.cmp",
+  opts = {
+    keymap = {
+      -- ["<Tab>"] = { "snippet_forward", "fallback" },
+    },
+    -- completion = {
+    --   menu = {
+    --     border = "",
+    --     scrollbar = false,
+    --     draw = {
+    --       align_to = "label",
+    --       columns = {
+    --         { "kind_icon" },
+    --         { "label", "label_description", gap = 1 },
+    --         { "kind" },
+    --         { "source_name" },
+    --       },
+    --     },
+    --   },
+    -- },
+
+    completion = {
+      menu = {
+        border = "none",
+        scrollbar = true,
+        draw = {
+          align_to = "label",
+          columns = {
+            { "kind_icon" },
+            { "label", "label_description", gap = 1 },
+            { "kind" },
+            { "source_name_3" },
+          },
+          components = {
+            source_name_3 = {
+              width = { max = 30 },
+              text = function(ctx) return " #" .. ctx.source_name:sub(1, 3) end,
+              highlight = "Comment",
+            },
+          },
+        },
+      },
+    },
+
+    appearance = {
+      nerd_font_variant = "mono",
+    },
+    cmdline = {
+      enabled = false,
+    },
+  },
+}
