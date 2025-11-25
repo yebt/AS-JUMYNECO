2c9f7ef84a697a5868c3717a9c176ad4df1e5990
Yahir Eduardo Bravo <yahir.eduardo.bravo@gmail.com>
2025-11-25 16:44:40 -0500

    feat(plugins/colorschemes): add some colors

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,39 @@
+return {
+
+  {
+    "AstroNvim/astroui",
+    ---@type AstroUIOpts
+    opts = {
+      -- colorscheme = "kanagawa",
+      -- colorscheme = "citruszest",
+      colorscheme = "cyberdream",
+      -- colorscheme = "everblush",
+    },
+  },
+
+  -- {
+  --   "zootedb0t/citruszest.nvim",
+  --   lazy = false,
+  --   priority = 1000,
+  --   opts = {
+  --     option = {
+  --       transparent = false, -- Enable/Disable transparency
+  --       bold = true,
+  --       italic = true,
+  --     },
+  --     -- Override default highlight style in this table
+  --     -- E.g If you want to override `Constant` highlight style
+  --     style = {
+  --       -- This will change Constant foreground color and make it bold.
+  --       -- Constant = { fg = "#FFFFFF", bold = true },
+  --     },
+  --   },
+  -- },
+
+  {
+    "scottmckendry/cyberdream.nvim",
+    lazy = false,
+    priority = 1000,
+  },
+
+}
