814fd2afa0563624d2e773574ea9de387805c95e
Yahir Eduardo Bravo <yahir.eduardo.bravo@gmail.com>
2025-11-26 15:19:07 -0500

    feat(plugins): add file to try load in lazy comunity plugins

---
+++ lua/plugins/tryloadlazy.lua
@@ -0,0 +1,22 @@
+return {
+  -- {
+  --   "echasnovski/mini.files",
+  --   event = { "VeryLazy" },
+  --   init = function()
+  --     vim.api.nvim_create_autocmd("BufEnter", {
+  --       group = vim.api.nvim_create_augroup("Neotree_start_directory", { clear = true }),
+  --       desc = "Start Neo-tree with directory",
+  --       once = true,
+  --       callback = function()
+  --         if package.loaded["mini.files"] then
+  --           return
+  --         else
+  --           local stats = vim.uv.fs_stat(vim.fn.argv(0))
+  --           if stats and stats.type == "directory" then require "mini.files" end
+  --         end
+  --       end,
+  --     })
+  --   end,
+  -- },
+
+}
