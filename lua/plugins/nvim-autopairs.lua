f7ac353133190c54b68ca2e4337400225b305a4c
Yahir Eduardo Bravo <yahir.eduardo.bravo@gmail.com>
2025-11-28 15:34:12 -0500

    fix(plugins/autopairs): change the way to load pairs

--- lua/plugins/nvim-autopairs.lua
+++ lua/plugins/nvim-autopairs.lua
@@ -1,52 +1,49 @@
 return {
   "windwp/nvim-autopairs",
-  config = function(_, opts)
+  config = function(plugin, opts)
+    require "astronvim.plugins.configs.nvim-autopairs"(plugin, opts)
+
     local npairs = require "nvim-autopairs"
-    npairs.setup(opts)
+    -- npairs.setup(opts)
     local Rule = require "nvim-autopairs.rule"
     local cond = require "nvim-autopairs.conds"
-
-    local brackets = { { "(", ")" }, { "[", "]" }, { "{", "}" } }
+    --
+    npairs.add_rules(
+      {
+        Rule("$", "$", { "tex", "latex" })
+          -- don't add a pair if the next character is %
+          :with_pair(cond.not_after_regex "%%")
+          -- don't add a pair if  the previous character is xxx
+          :with_pair(cond.not_before_regex("xxx", 3))
+          -- don't move right when repeat character
+          :with_move(cond.none())
+          -- don't delete if the next character is xx
+          :with_del(cond.not_after_regex "xx")
+          -- disable adding a newline when you press <cr>
+          :with_cr(cond.none()),
+      },
+      -- disable for .vim files, but it work for another filetypes
+      Rule("a", "a", "-vim")
+    )
     npairs.add_rules {
-      -- Rule for a pair with left-side ' ' and right side ' '
-      Rule(" ", " ")
-        -- Pair will only occur if the conditional function returns true
-        :with_pair(function(opts)
-          -- We are checking if we are inserting a space in (), [], or {}
-          local pair = opts.line:sub(opts.col - 1, opts.col)
-          return vim.tbl_contains({
-            brackets[1][1] .. brackets[1][2],
-            brackets[2][1] .. brackets[2][2],
-            brackets[3][1] .. brackets[3][2],
-          }, pair)
-        end)
-        :with_move(cond.none())
-        :with_cr(cond.none())
-        -- We only want to delete the pair of spaces when the cursor is as such: ( | )
-        :with_del(function(opts)
-          local col = vim.api.nvim_win_get_cursor(0)[2]
-          local context = opts.line:sub(col - 1, col + 2)
-          return vim.tbl_contains({
-            brackets[1][1] .. "  " .. brackets[1][2],
-            brackets[2][1] .. "  " .. brackets[2][2],
-            brackets[3][1] .. "  " .. brackets[3][2],
-          }, context)
-        end),
+      -- specify a list of rules to add
+      -- Rule(" ", " "):with_pair(function(options) return false end),
+      Rule(" ", " "):with_pair(function(options)
+        local pair = options.line:sub(options.col - 1, options.col)
+        return vim.tbl_contains({ "()", "[]", "{}" }, pair)
+      end),
+      Rule("( ", " )")
+        :with_pair(function() return false end)
+        :with_move(function(options) return options.prev_char:match ".%)" ~= nil end)
+        :use_key ")",
+      Rule("{ ", " }")
+        :with_pair(function() return false end)
+        :with_move(function(options) return options.prev_char:match ".%}" ~= nil end)
+        :use_key "}",
+      Rule("[ ", " ]")
+        :with_pair(function() return false end)
+        :with_move(function(options) return options.prev_char:match ".%]" ~= nil end)
+        :use_key "]",
     }
-    -- For each pair of brackets we will add another rule
-    for _, bracket in pairs(brackets) do
-      npairs.add_rules {
-        -- Each of these rules is for a pair with left-side '( ' and right-side ' )' for each bracket type
-        Rule(bracket[1] .. " ", " " .. bracket[2])
-          :with_pair(cond.none())
-          :with_move(function(opts) return opts.char == bracket[2] end)
-          :with_del(cond.none())
-          :use_key(bracket[2])
-          -- Removes the trailing whitespace that can occur without this
-          :replace_map_cr(
-            function(_) return "<C-c>2xi<CR><C-c>O" end
-          ),
-      }
-    end
   end,
 }
