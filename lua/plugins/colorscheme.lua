a803221f143160f4578040d38574bba4f014fb64
Yahir Eduardo Bravo <yahir.eduardo.bravo@gmail.com>
2025-12-01 17:57:18 -0500

    feat(plugins): add colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,14 +1,16 @@
+-- if true then return {} end --- WARNING: remove to activate the rest
+
 return {
 
   {
     "AstroNvim/astroui",
     ---@type AstroUIOpts
     opts = {
-      colorscheme = "astrodark",
+      -- colorscheme = "astrodark",
       -- colorscheme = "tokyonight",
       -- colorscheme = "kanagawa",
       -- colorscheme = "citruszest",
-      -- colorscheme = "cyberdream",
+      colorscheme = "cyberdream",
       -- colorscheme = "onedark",
       -- colorscheme = "kanagawa",
       -- colorscheme = "everblush",
@@ -80,6 +82,7 @@ return {
     },
   },
 
+  -- CD
   {
     "scottmckendry/cyberdream.nvim",
     lazy = true,
@@ -87,6 +90,7 @@ return {
     -- priority = 1000,
   },
 
+  -- OD
   {
     "navarasu/onedark.nvim",
     lazy = true,
@@ -110,8 +114,10 @@ return {
     end,
   },
 
+  -- V
   {
     "https://codeberg.org/pdewey/vitesse-nvim",
+    lazy = false,
     priority = 1000,
   },
 
@@ -123,60 +129,23 @@ return {
     },
   },
 
-  -- {
-  --   "loctvl842/monokai-pro.nvim",
-  --   lazy = false,
-  --   priority = 1000,
-  --   opts = {
-  --     transparent_background = false,
-  --     terminal_colors = true,
-  --     devicons = true, -- highlight the icons of `nvim-web-devicons`
-  --     styles = {
-  --       comment = { italic = true },
-  --       keyword = { italic = true }, -- any other keyword
-  --       type = { italic = true }, -- (preferred) int, long, char, etc
-  --       storageclass = { italic = true }, -- static, register, volatile, etc
-  --       structure = { italic = true }, -- struct, union, enum, etc
-  --       parameter = { italic = true }, -- parameter pass in function
-  --       annotation = { italic = true },
-  --       tag_attribute = { italic = true }, -- attribute of tag in reactjs
-  --     },
-  --     filter = "pro", -- classic | octagon | pro | machine | ristretto | spectrum
-  --     -- Enable this will disable filter option
-  --     day_night = {
-  --       enable = false, -- turn off by default
-  --       day_filter = "pro", -- classic | octagon | pro | machine | ristretto | spectrum
-  --       night_filter = "spectrum", -- classic | octagon | pro | machine | ristretto | spectrum
-  --     },
-  --     inc_search = "background", -- underline | background
-  --     background_clear = {
-  --       -- "float_win",
-  --       "toggleterm",
-  --       "telescope",
-  --       -- "which-key",
-  --       "renamer",
-  --       "notify",
-  --       -- "nvim-tree",
-  --       -- "neo-tree",
-  --       -- "bufferline", -- better used if background of `neo-tree` or `nvim-tree` is cleared
-  --     }, -- "float_win", "toggleterm", "telescope", "which-key", "renamer", "neo-tree", "nvim-tree", "bufferline"
-  --     plugins = {
-  --       bufferline = {
-  --         underline_selected = false,
-  --         underline_visible = false,
-  --       },
-  --       indent_blankline = {
-  --         context_highlight = "default", -- default | pro
-  --         context_start_underline = false,
-  --       },
-  --     },
-  --     ---@param c Colorscheme
-  --     override = function(c) end,
-  --     ---@param cs Colorscheme
-  --     ---@param p ColorschemeOptions
-  --     ---@param Config MonokaiProOptions
-  --     ---@param hp Helper
-  --     -- override = function(cs: Colorscheme, p: ColorschemeOptions, Config: MonokaiProOptions, hp: Helper) end,
-  --   },
-  -- },
+  {
+    "zootedb0t/citruszest.nvim",
+    lazy = true,
+    opts = {
+      option = {
+        transparent = false, -- Enable/Disable transparency
+        bold = false,
+        italic = true,
+      },
+      -- Override default highlight style in this table
+      -- E.g If you want to override `Constant` highlight style
+      style = {
+        -- This will change Constant foreground color and make it bold.
+        Constant = { fg = "#FFFFFF", bold = true },
+      },
+    },
+  },
+
+  --
 }
