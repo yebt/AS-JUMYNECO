331895b71d20263c0f8f0489fc4050ee4d53c49b
Yahir Eduardo Bravo <yahir.eduardo.bravo@gmail.com>
2026-01-09 11:38:15 -0500

    feat: add autohide tabline

--- lua/plugins/astrocore.lua
+++ lua/plugins/astrocore.lua
@@ -56,6 +56,7 @@ return {
         --   vim.fs.joinpath(vim.fn.stdpath "config", "spell/es.utf-8.add"),
         -- },
         exrc = true,
+        -- showtabline = 0,
       },
       g = { -- vim.g.<key>
         -- ["spellfile_URL"] = "https://ftp.nluug.nl/vim/runtime/spell/",
@@ -93,6 +94,33 @@ return {
 
         -- setting a mapping to false will disable it
         -- ["<C-S>"] = false,
+      },
+    },
+
+    autocmds = {
+      -- autocommands are organized into augroups for easy management
+      autohidetabline = {
+        -- each augroup contains a list of auto commands
+        {
+          -- create a new autocmd on the "User" event
+          event = "User",
+          -- the pattern is the name of our User autocommand events
+          pattern = "AstroBufsUpdated", -- triggered when vim.t.bufs is updated
+          -- nice description
+          desc = "Hide tabline when only one buffer and one tab",
+          -- add the autocmd to the newly created augroup
+          group = "autohidetabline",
+          callback = function()
+            -- if there is more than one buffer in the tab, show the tabline
+            -- if there are 0 or 1 buffers in the tab, only show the tabline if there is more than one vim tab
+            local new_showtabline = #vim.t.bufs > 1 and 2 or 1
+            -- check if the new value is the same as the current value
+            if new_showtabline ~= vim.opt.showtabline:get() then
+              -- if it is different, then set the new `showtabline` value
+              vim.opt.showtabline = new_showtabline
+            end
+          end,
+        },
       },
     },
   },
