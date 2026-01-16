15771cbe017e6afe4b5957a36a34364812cfbd97
Yahir Eduardo Bravo <yahir.eduardo.bravo@gmail.com>
2026-01-16 13:41:29 -0500

    feat: add winbar with the name of the fiule

--- lua/plugins/heirline.lua
+++ lua/plugins/heirline.lua
@@ -1,35 +1,25 @@
 return {
   "rebelot/heirline.nvim",
   opts = function(_, opts)
-    local status = require "astroui.status"
+    -- local status = require "astroui.status"
     --
     --- deactive breadcrumbs
-    opts.winbar = nil
-
-    --   opts.winbar = { -- winbar
-    --     init = function(self) self.bufnr = vim.api.nvim_get_current_buf() end,
-    --     fallthrough = false,
-    --     { -- inactive winbar
-    --       -- condition = function() return not status.condition.is_active() end,
-    --       status.component.separated_path(),
-    --       status.component.file_info {
-    --         file_icon = {
-    --           -- hl = status.hl.file_icon "winbar",
-    --           -- padding = { left = 0 },
-    --         },
-    --         filename = {},
-    --         filetype = false,
-    --         file_read_only = false,
-    --         -- hl = status.hl.get_attributes("winbarnc", true),
-    --         surround = false,
-    --         update = "BufEnter",
-    --       },
-    --     },
-    --     { -- active winbar
-    --       -- status.component.breadcrumbs {
-    --       --   hl = status.hl.get_attributes("winbar", true),
-    --       -- },
-    --     },
-    --   }
+    opts.tabline = false
+    opts.winbar = {
+      {
+        provider = function()
+          local winbar_components = {
+            "%=",
+            " >> ",
+            [[%#Comment#%<%{expand("%:~:.:h")}%{%(bufname() !=# '' ? '/' : '')%}]],
+            [[%#Constant#%t%#ModeMsg#%{%(bufname() !=# '' ? ' %y' : '')%}]],
+            "%H%W%M%R%#Normal#",
+            -- [[%#Normal#%{%get(b:,'stl_filetype_icon','··')%}]],
+          }
+          return table.concat(winbar_components, "%#Statusline#")
+        end,
+      },
+    }
+    -- opts.statusline = {}
   end,
 }
