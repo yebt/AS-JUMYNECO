cef68ab4b9ab0827c0ba586f66a33123cb2736c6
Yahir Eduardo Bravo <yahir.eduardo.bravo@gmail.com>
2026-01-28 09:06:27 -0500

    feat(plugin): add configs for snipe

--- lua/plugins/buffers.lua
+++ lua/plugins/buffers.lua
@@ -5,6 +5,14 @@ return {
     keys = {
       { "<M-m>", function() require("snipe").open_buffer_menu() end, desc = "Open Snipe buffer menu" },
     },
-    opts = {},
+    opts = {
+
+      preselect_current = true,
+      preselect = function() return require("snipe").preselect_by_classifier "#" end,
+
+      hints = {
+        dictionary = "asdflewcmpghio",
+      },
+    },
   },
 }
