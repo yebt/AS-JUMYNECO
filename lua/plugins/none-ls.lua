53b1e6fe619b4c894ea583b199bfd32531df2876
Yahir Eduardo Bravo <yahir.eduardo.bravo@gmail.com>
2026-02-02 08:50:06 -0500

    feat: add try to format with oxfmt

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -6,6 +6,59 @@ if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 return {
   "nvimtools/none-ls.nvim",
   opts = function(_, opts)
+    local null_ls = require "null-ls"
+    local helpers = require "null-ls.helpers"
+    local methods = require "null-ls.methods"
+
+    local oxfmt = helpers.make_builtin {
+      name = "oxfmt",
+      meta = {
+        url = "https://github.com/oxc-project/oxc",
+        description = "The Oxidation Compiler is a collection of high-performance tools for JavaScript and TypeScript written in Rust.",
+      },
+      method = { methods.internal.FORMATTING },
+      filetypes = {
+        "vue",
+        "typescript",
+        "typescriptreact",
+        "javascript",
+        "javascriptreact",
+        "vuejs",
+        "javascript",
+        "javascriptreact",
+        "typescript",
+        "typescriptreact",
+        "vue",
+        "css",
+        "scss",
+        "less",
+        "html",
+        "json",
+        "jsonc",
+        "yaml",
+        "markdown",
+        "markdown.mdx",
+        "graphql",
+        "handlebars",
+        "svelte",
+        "astro",
+        "htmlangular",
+      },
+      generator_opts = {
+        command = "bunx oxfmt",
+        -- arg = helpers.range_formatting_args_factory({
+        --   "--stdin",
+        -- })
+        args = { "--stdin-file-path", "$FILENAME" },
+        to_stdin = true,
+      },
+      factory = helpers.formatter_factory,
+    }
+
+    null_ls.register(oxfmt)
+    --
+    --
+
     -- opts variable is the default configuration table for the setup function call
     -- local null_ls = require "null-ls"
 
@@ -16,9 +69,27 @@ return {
     -- Only insert new sources, do not replace the existing ones
     -- (If you wish to replace, use `opts.sources = {}` instead of the `list_insert_unique` function)
     opts.sources = require("astrocore").list_insert_unique(opts.sources, {
+      -- oxfmt,
+      -- null_ls.get_source "oxfmt",
       -- Set a formatter
       -- null_ls.builtins.formatting.stylua,
       -- null_ls.builtins.formatting.prettier,
     })
+
+    --
+    -- local oxfmt = helpers.make_builtin {
+    --   name = "oxfmt",
+    --   method = methods.internal.FORMATTING,
+    --   filetypes = { "ocaml", "reason", "vue" }, -- ajusta según corresponda
+    --   generator_opts = {
+    --     command = "oxfmt",
+    --     args = { "--stdin" },
+    --     to_stdin = true,
+    --   },
+    --   factory = helpers.formatter_factory,
+    -- }
+    --
+    -- opts.sources = opts.sources or {}
+    -- table.insert(opts.sources, oxfmt)
   end,
 }
