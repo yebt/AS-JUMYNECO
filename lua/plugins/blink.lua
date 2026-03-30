e59aa96b4c34b65fc3a15a021f9f3a7623455fd5
Yahir Eduardo Bravo <yahir.eduardo.bravo@gmail.com>
2026-03-30 15:21:35 -0500

    feat(plugins): change map for blink

---
+++ lua/plugins/blink.lua
@@ -0,0 +1,42 @@
+
+return { -- override blink.cmp plugin
+  "saghen/blink.cmp",
+  opts = {
+    keymap = {
+      ["<Tab>"] = { "snippet_forward", "fallback" },
+      -- [""] = { function(cmp) cmp.show { providers = { "ripgrep" } } end },
+      ["<M-k>"] = { "show_signature", "fallback" },
+      ["<C-L>"] = {
+        function(cmp)
+          cmp.show {
+            providers = { "snippets" },
+            initial_selected_item_idx = 1,
+          }
+        end,
+      },
+    },
+    completion = {
+      menu = {
+        border = "none",
+        scrollbar = true,
+        draw = {
+          align_to = "label",
+          columns = {
+            { "kind_icon" },
+            { "label", "label_description", gap = 1 },
+            { "kind" },
+            { "source_name_3" },
+          },
+          components = {
+            source_name_3 = {
+              width = { max = 30 },
+              text = function(ctx) return " #" .. ctx.source_name:sub(1, 3) end,
+              highlight = "Comment",
+            },
+          },
+        },
+      },
+    },
+
+  },
+}
