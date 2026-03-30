98f9282be3e8306826be49e8d7fc28ebd236ce79
Yahir Eduardo Bravo <yahir.eduardo.bravo@gmail.com>
2026-03-30 15:04:36 -0500

    feat(plugin): add changes in config from old to actual

--- lua/plugins/mappings.lua
+++ lua/plugins/mappings.lua
@@ -3,231 +3,84 @@ return {
     "AstroNvim/astrocore",
     ---@type AstroCoreOpts
     opts = {
-
       mappings = {
+        -- first key is the mode
         n = {
-          ["<Leader>o"] = { "o<esc>O<cr>", desc = "Add line beetween below" },
-          ["<Leader>O"] = { "O<esc>O<cr>", desc = "Add line beetween abobe" },
-          --
           ["<Leader>a"] = { "ggVG", desc = "Select All" },
           ["<Leader>y"] = { '"+y', desc = "Copy to clipboard" },
           ["<Leader>Y"] = { '"+y$', desc = "Copy to clipboard" },
-          --
-          -- ["<C-k><C-w>"] = {
-          --   function()
-          --     require("astrocore.buffer").close_all()
-          --     -- vim.cmd "bd"
-          --     -- local bufs = vim.fn.getbufinfo { buflisted = 1 }
-          --     -- if not bufs[2] then require("snacks").dashboard() end
-          --   end,
-          --   desc = "Close all buffers",
-          -- },
-          --
-          ["<M-b>"] = {
-            function() require("snacks").explorer() end,
-            desc = "Open files with snack",
-          },
-          ["<M-r>"] = {
-            function() require("snacks").explorer.reveal() end,
-            desc = "Open files with snack",
-          },
-          ["<Tab><Tab>"] = {
+
+          -- second key is the lefthand side of the map
+          -- mappings seen under group name "Buffer"
+          ["<Leader>bD"] = {
             function()
-              require("snacks").picker.buffers {
-                -- layout = { preset = "sidebar", layout = { position = "right" } },
-                layout = {
-                  preset = "vscode",
-                },
-              }
+              require("astroui.status").heirline.buffer_picker(function(bufnr)
+                require("astrocore.buffer").close(bufnr)
+              end)
             end,
-            desc = "Find buffers",
+            desc = "Pick to close",
           },
-          --
-          ["<Leader>rw"] = {
-            ":%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left><Left>",
-            desc = "Replace cursor word in the document",
-          },
-          --
-          ["<M-a>"] = { function() require("astrocore.buffer").nav(-vim.v.count1) end, desc = "Next buffer" },
-          ["<M-d>"] = { function() require("astrocore.buffer").nav(vim.v.count1) end, desc = "Next buffer" },
-          ["<M-s>"] = { function() require("astrocore.buffer").prev() end, desc = "Go to previous buffer" },
-          ["<M-c>"] = {
-            function()
-              local bufs = vim.fn.getbufinfo { buflisted = 1 }
-              require("astrocore.buffer").close(0)
-              -- if not bufs[2] then require("snacks").dashboard() end
-            end,
-            desc = "Close buffer",
-          },
-          ---
-          -- ["<M-c>"] = { "<Leader>c", desc = "Delete buffer" },
-          -- ["<M-d>"] = { "]b", desc = "Delete buffer" },
-          -- ["<M-s>"] = { "<cmd>b#<cr>", desc = "Delete buffer" },
-          ["¥"] = {
-            function()
-              if not require("mini.files").close() then require("mini.files").open() end
-            end,
-            desc = "Explorer",
-          },
-          ["~"] = {
-            function()
-              if not require("mini.files").close() then
-                require("mini.files").open(vim.api.nvim_buf_get_name(0), false)
-              end
-            end,
-            desc = "Explorer in filepath",
-          },
-          ["¿"] = {
-            function()
-              if not require("mini.files").close() then
-                require("mini.files").open(vim.api.nvim_buf_get_name(0), false)
-              end
-            end,
-            desc = "Explorer in filepath",
-          },
-          ["×"] = {
-            function()
-              if not require("mini.files").close() then
-                require("mini.files").open(vim.api.nvim_buf_get_name(0), false)
-              end
-            end,
-            desc = "Explorer",
-          },
+
           ["<C-p>"] = {
             function()
               require("snacks").picker.files {
-                prompt = " ",
-                show_delay = 10,
                 layout = {
-                  cycle = true,
-                  -- preview = nil,
-                  hidden = { "preview" },
+                  preset = "vscode",
                   layout = {
-                    box = "horizontal",
                     backdrop = 30,
-                    -- width = 0.8,
-                    width = function() return vim.o.columns >= 120 and 0.6 or 0.9 end,
-                    height = 0.4,
-                    border = "none",
-                    {
-                      box = "vertical",
-                      {
-                        win = "input",
-                        title = "{title} {live} {flags}",
-                        title_pos = "center",
-                        border = "rounded",
-                        height = 1,
-                      },
-                      { win = "list", title = " Results ", title_pos = "center", border = "none" },
-                    },
-                    {
-                      win = "preview",
-                      title = "{preview:Preview}",
-                      width = 0.45,
-                      -- border = "rounded",
-                      border = "none",
-                      title_pos = "center",
-                    },
-                  },
+                    width = function() return vim.o.co >= 120 and 0.6 or 0.9 end ,
+                    height = 0.5
+                  }
                 },
-
                 formatters = {
                   file = {
                     filename_first = true,
                     truncate = "center",
                     git_status_hl = false,
-                  },
-                },
+                  }
+                }
               }
             end,
+            desc = "Pick file"
           },
-          ["<M-o>"] = {
-            function()
-              require("snacks").picker.lsp_symbols {
-                prompt = " ",
-                show_delay = 10,
-                layout = {
-                  cycle = true,
-                  -- preview = nil,
-                  hidden = { "preview" },
-                  layout = {
-                    box = "horizontal",
-                    backdrop = 30,
-                    -- width = 0.8,
-                    width = function() return vim.o.columns >= 120 and 0.6 or 0.9 end,
-                    height = 0.8,
-                    border = "none",
-                    {
-                      box = "vertical",
-                      {
-                        win = "input",
-                        title = "{title} {live} {flags}",
-                        title_pos = "center",
-                        border = "rounded",
-                        height = 1,
-                      },
-                      { win = "list", title = " Results ", title_pos = "center", border = "none" },
-                    },
-                    {
-                      win = "preview",
-                      title = "{preview:Preview}",
-                      width = 0.45,
-                      -- border = "rounded",
-                      border = "none",
-                      title_pos = "center",
-                    },
-                  },
-                },
 
-                formatters = {
-                  file = {
-                    filename_first = true,
-                    truncate = "center",
-                    git_status_hl = false,
-                  },
-                },
-              }
-            end,
-          },
-          ["<Leader>R"] = {
-            function() require("snacks").picker.recent {} end,
-          },
-          -- ["<M-z><M-z>"] = {
-          --   function() vim.opt.wrap = not vim.opt.wrap._value end,
-          --   desc = "Toggle wrap text",
-          -- },
         },
-        x = {
-          ["<Leader>y"] = { '"+y', desc = "Copy to clipboard" },
+          ["<C-o>"] = {
+            function()
+              require("snacks").picker.lsp_symbols()
+            end,
+            desc = "Pick lsp symbol"
+          }
+        t = {
+          -- setting a mapping to false will disable it
+          -- ["<esc>"] = false,
         },
       },
     },
   },
-
-  -- lsp
-  -- {
-  --   "AstroNvim/astrolsp",
-  --   ---@type AstroLSPOpts
-  --   opts = {
-  --     mappings = {
-  --       n = {
-  --         -- this mapping will only be set in buffers with an LSP attached
-  --         K = {
-  --           function()
-  --             vim.lsp.buf.hover()
-  --           end,
-  --           desc = "Hover symbol details",
-  --         },
-  --         -- condition for only server with declaration capabilities
-  --         gD = {
-  --           function()
-  --             vim.lsp.buf.declaration()
-  --           end,
-  --           desc = "Declaration of current symbol",
-  --           cond = "textDocument/declaration",
-  --         },
-  --       },
-  --     },
-  --   },
-  -- },
+  {
+    "AstroNvim/astrolsp",
+    ---@type AstroLSPOpts
+    opts = {
+      mappings = {
+        n = {
+          -- this mapping will only be set in buffers with an LSP attached
+          K = {
+            function()
+              vim.lsp.buf.hover()
+            end,
+            desc = "Hover symbol details",
+          },
+          -- condition for only server with declaration capabilities
+          gD = {
+            function()
+              vim.lsp.buf.declaration()
+            end,
+            desc = "Declaration of current symbol",
+            cond = "textDocument/declaration",
+          },
+        },
+      },
+    },
+  },
 }
--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -1,4 +1,4 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+-- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 
 -- You can also add or configure plugins by creating files in this `plugins/` folder
 -- PLEASE REMOVE THE EXAMPLES YOU HAVE NO INTEREST IN BEFORE ENABLING THIS FILE
@@ -9,7 +9,7 @@ return {
 
   -- == Examples of Adding Plugins ==
 
-  "andweeb/presence.nvim",
+  -- "andweeb/presence.nvim",
   {
     "ray-x/lsp_signature.nvim",
     event = "BufRead",
@@ -25,17 +25,7 @@ return {
       dashboard = {
         preset = {
           header = table.concat({
-            " █████  ███████ ████████ ██████   ██████ ",
-            "██   ██ ██         ██    ██   ██ ██    ██",
-            "███████ ███████    ██    ██████  ██    ██",
-            "██   ██      ██    ██    ██   ██ ██    ██",
-            "██   ██ ███████    ██    ██   ██  ██████ ",
-            "",
-            "███    ██ ██    ██ ██ ███    ███",
-            "████   ██ ██    ██ ██ ████  ████",
-            "██ ██  ██ ██    ██ ██ ██ ████ ██",
-            "██  ██ ██  ██  ██  ██ ██  ██  ██",
-            "██   ████   ████   ██ ██      ██",
+            "HOPE"
           }, "\n"),
         },
       },
@@ -43,16 +33,18 @@ return {
   },
 
   -- You can disable default plugins as follows:
-  { "max397574/better-escape.nvim", enabled = false },
+  -- { "max397574/better-escape.nvim", enabled = false },
 
   -- You can also easily customize additional setup of plugins that is outside of the plugin's setup call
   {
     "L3MON4D3/LuaSnip",
     config = function(plugin, opts)
-      require "astronvim.plugins.configs.luasnip"(plugin, opts) -- include the default astronvim config that calls the setup call
       -- add more custom luasnip configuration such as filetype extend or custom snippets
       local luasnip = require "luasnip"
       luasnip.filetype_extend("javascript", { "javascriptreact" })
+
+      -- include the default astronvim config that calls the setup call
+      require "astronvim.plugins.configs.luasnip"(plugin, opts)
     end,
   },
 
