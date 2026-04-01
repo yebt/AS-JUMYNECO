e1b9d3e9f9fda88098777904a14128ed0e25082e
Yahir <yahir.eduardo.bravo@gmail.com>
2026-04-01 14:38:58 -0500

    feat(plugins): change default clipboard and add some defualt lsps to install

---
+++ lua/plugins/mason-tool-installer.lua
@@ -0,0 +1,14 @@
+return {
+  "WhoIsSethDaniel/mason-tool-installer.nvim",
+  opts = {
+    ensure_installed = {
+      "gopls",
+      "oxfmt",
+      "oxlint",
+      "jq",
+      "eslint-lsp",
+      "prettier",
+      "intelephense",
+    },
+  },
+}
