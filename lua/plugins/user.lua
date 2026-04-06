14ec19cc0c3fb1706c72d43d19637f918dcdcbb4
Yahir <yahir.eduardo.bravo@gmail.com>
2026-04-05 22:54:11 -0500

    feat: allow snippets loading fro luasnipps and friendly snippets

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -41,10 +41,15 @@ return {
     config = function(plugin, opts)
       -- add more custom luasnip configuration such as filetype extend or custom snippets
       local luasnip = require "luasnip"
-      luasnip.filetype_extend("javascript", { "javascriptreact" })
+      -- luasnip.filetype_extend("javascript", { "javascriptreact" })
 
       -- include the default astronvim config that calls the setup call
       require "astronvim.plugins.configs.luasnip"(plugin, opts)
+
+      -- load snippets paths
+      require("luasnip.loaders.from_vscode").lazy_load({
+        paths = { vim.fn.stdpath("config") .. "/snippets" },
+      })
     end,
   },
 
