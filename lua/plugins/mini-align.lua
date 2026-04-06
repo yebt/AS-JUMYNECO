2853ee0ac497c18c4400d2366793496a40c64842
Yahir Eduardo Bravo <yahir.eduardo.bravo@gmail.com>
2026-04-06 10:17:13 -0500

    feat(plugins): add mini align

---
+++ lua/plugins/mini-align.lua
@@ -0,0 +1,15 @@
+return {
+  "nvim-mini/mini.align",
+  version = false,
+  opt = {
+    mappings = {
+      start = "ga",
+      start_with_preview = "gA",
+    },
+  },
+  config = true,
+  -- keys = {
+  --   {"ga", mode = {"x","n"}, desc = "Align text"},
+  --   {"gA", mode = {"x","n"}, desc = "Align text"},
+  -- }
+}
