e93ecca757678dd86ad52a5f8ea8847eb3b1c258
Yahir Eduardo Bravo <yahir.eduardo.bravo@gmail.com>
2026-06-27 12:04:57 -0500

    feat: reset

--- lua/plugins/colorschemes.lua
+++ lua/plugins/colorschemes.lua
@@ -8,7 +8,7 @@ return {
       -- colorscheme = "astrodark",
       -- colorscheme = "catppuccin-mocha",
       -- colorscheme = "catppuccin",
-      colorscheme = "catppuccin-nvim",
+      -- colorscheme = "catppuccin-nvim",
       -- colorscheme = "solarized-osaka",
       -- colorscheme = "koda",
       -- colorscheme = "thorn",
@@ -16,6 +16,31 @@ return {
       -- colorscheme = "tokyonight",
       -- colorscheme = "oxocarbon",
       -- colorscheme = "oasis",
+    },
+  },
+
+  {
+    "AstroNvim/astrotheme",
+    opts = {
+      palette = "astrodark",
+      style = {
+        transparent = false, -- Bool value, toggles transparency.
+        inactive = true, -- Bool value, toggles inactive window color.
+        float = true, -- Bool value, toggles floating windows background colors.
+        neotree = true, -- Bool value, toggles neo-trees background color.
+        border = true, -- Bool value, toggles borders.
+        title_invert = true, -- Bool value, swaps text and background colors.
+        italic_comments = true, -- Bool value, toggles italic comments.
+        simple_syntax_colors = true, -- Bool value, simplifies the amounts of colors used for syntax highlighting.
+      },
+      palettes = {
+        astrodark = {
+          ui = {
+            -- background = "#1A1D23"
+            base = "#00151B"
+          },
+        },
+      },
     },
   },
 
@@ -118,36 +143,36 @@ return {
     priority = 1000,
     opts = {
       flavour = "mocha", -- latte, frappe, macchiato, mocha
-      color_overrides = {
-        mocha = {
-          rosewater = "#f5e0dc",
-          flamingo  = "#f2cdcd",
-          pink      = "#f5c2e7",
-          mauve     = "#cba6f7",
-          red       = "#f38ba8",
-          maroon    = "#eba0ac",
-          peach     = "#fab387",
-          yellow    = "#f9e2af",
-          green     = "#a6e3a1",
-          teal      = "#94e2d5",
-          sky       = "#89dceb",
-          sapphire  = "#74c7ec",
-          blue      = "#89b4fa",
-          lavender  = "#b4befe",
-          text      = "#FEFFFE",
-          subtext1  = "#FDF6E3",
-          subtext0  = "#EFE9D5",
-          overlay2  = "#9399b2",
-          overlay1  = "#7f849c",
-          overlay0  = "#6c7086",
-          surface2  = "#585b70",
-          surface1  = "#455159",
-          surface0  = "#324145",
-          base      = "#00151B",
-          mantle    = "#012029",
-          crust     = "#012B36",
-        },
-      },
+      -- color_overrides = {
+      --   mocha = {
+      --     rosewater = "#f5e0dc",
+      --     flamingo  = "#f2cdcd",
+      --     pink      = "#f5c2e7",
+      --     mauve     = "#cba6f7",
+      --     red       = "#f38ba8",
+      --     maroon    = "#eba0ac",
+      --     peach     = "#fab387",
+      --     yellow    = "#f9e2af",
+      --     green     = "#a6e3a1",
+      --     teal      = "#94e2d5",
+      --     sky       = "#89dceb",
+      --     sapphire  = "#74c7ec",
+      --     blue      = "#89b4fa",
+      --     lavender  = "#b4befe",
+      --     text      = "#FEFFFE",
+      --     subtext1  = "#FDF6E3",
+      --     subtext0  = "#EFE9D5",
+      --     overlay2  = "#9399b2",
+      --     overlay1  = "#7f849c",
+      --     overlay0  = "#6c7086",
+      --     surface2  = "#585b70",
+      --     surface1  = "#455159",
+      --     surface0  = "#324145",
+      --     base      = "#00151B",
+      --     mantle    = "#012029",
+      --     crust     = "#012B36",
+      --   },
+      -- },
     },
   },
 
