46b64ca9d07a872a70a16326415b623e90a9c2bd
Yahir Eduardo Bravo <yahir.eduardo.bravo@gmail.com>
2026-07-14 16:26:48 -0500

    format

--- lsp/vtsls.lua
+++ lsp/vtsls.lua
@@ -4,13 +4,14 @@
 local vue_language_server_path = vim.fn.expand "$MASON/packages"
   .. "/vue-language-server"
   .. "/node_modules/@vue/language-server"
-local tsserver_filetypes = { 'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue' }
+
+local tsserver_filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" }
 
 local vue_plugin = {
-  name = '@vue/typescript-plugin',
+  name = "@vue/typescript-plugin",
   location = vue_language_server_path,
-  languages = { 'vue' },
-  configNamespace = 'typescript',
+  languages = { "vue" },
+  configNamespace = "typescript",
 }
 
 return {
